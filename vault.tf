105800286611a46691e08b77787a790abebec836
David Joo <david.joo@hashicorp.com>
2024-10-29 12:06:51 +1100

    reseource names updated

--- vault.tf
+++ vault.tf
@@ -1,11 +1,11 @@
-resource "hcp_hvn" "learn_hcp_vault_hvn" {
+resource "hcp_hvn" "djoo_hcp_vault_demo_hvn" {
   hvn_id         = var.hvn_id
   cloud_provider = var.cloud_provider
   region         = var.region
 }
 
 resource "hcp_vault_cluster" "learn_hcp_vault" {
-  hvn_id     = hcp_hvn.learn_hcp_vault_hvn.hvn_id
+  hvn_id     = hcp_hvn.djoo_hcp_vault_demo_hvn.hvn_id
   cluster_id = var.cluster_id
   tier       = var.tier
   # public_endpoint = true
